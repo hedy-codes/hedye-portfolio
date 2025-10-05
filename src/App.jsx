e2f7eca8a0fcdd3f0c13d3607b8a42493eb74a04
Hedye Azizi <hedyeazizi0752@gmail.com>
2025-10-06 00:22:23 +0330

    feat(router): setup basic routing with layout placeholder

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,13 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
-      <h1 className="text-red-800 text-9xl italic flex justify-center">
-        hello
-      </h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<h1>this is layout</h1>} />
+        </Routes>
+      </BrowserRouter>
     </>
   );
 }
