afcf7026755879110ef1a1f1ae0c9439f1ff849d
Hedye Azizi <hedyeazizi0752@gmail.com>
2025-10-06 22:41:55 +0330

    style: apply Tailwind CSS classes for Header layout and design

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,9 +4,9 @@ import { Link } from "react-router-dom";
 export default function Header() {
   return (
     <div>
-      <header>
+      <header className="w-[600px] h-[70px] py-3 mt-7 mx-auto rounded-lg flex items-center justify-center gap-20 text-white bg-blue-800 shadow-lg shadow-blue-600">
         <Link to={"/"}>
-          <FaHome />
+          <FaHome className="text-3xl" />
         </Link>
         <Link to={"/skills"}>مهارت ها </Link>
         <Link to={"/projects"}>پروژه ها </Link>
