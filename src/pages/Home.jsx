14fe1a4c1f4003604b81974be541c58a2e87f5fa
Hedye Azizi <hedyeazizi0752@gmail.com>
2025-10-16 00:37:10 +0330

    fix: correct profile image path in Home page

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,7 +5,7 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center mt-20">
       <div className="w-[400px]">
-        <img src="/img/img.png" alt="logo" className="rounded-[70px]" />
+        <img src="/img/image.png" alt="logo" className="rounded-[70px]" />
       </div>
       <div className=" w-[400px] flex items-center justify-center flex-col gap-5">
         <h1 className="text-[60px] font-bold text-blue-800">هدیه عزیزی</h1>
