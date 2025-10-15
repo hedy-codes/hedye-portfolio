eb5ef96ca04a1025b5a9a688e81a1a8edaba920f
Hedye Azizi <hedyeazizi0752@gmail.com>
2025-10-16 00:00:40 +0330

    feat: add two-column structure for Home layout

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div className="flex items-center justify-center mt-20"></div>;
+  return (
+    <div className="flex items-center justify-center mt-20">
+      <div className="w-[400px]"></div>
+      <div className=" w-[400px]"></div>
+    </div>
+  );
 }
