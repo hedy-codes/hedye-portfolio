9d242b2dd7a633f671d88caf7710e7a45b2934a6
Hedye Azizi <hedyeazizi0752@gmail.com>
2025-10-16 00:35:42 +0330

    style: apply flex column layout with gap to Home text section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -5,9 +5,9 @@ export default function Home() {
   return (
     <div className="flex items-center justify-center mt-20">
       <div className="w-[400px]">
-        <img src="/img/image.png" alt="logo" className="rounded-[70px]" />
+        <img src="/img/img.png" alt="logo" className="rounded-[70px]" />
       </div>
-      <div className=" w-[400px]">
+      <div className=" w-[400px] flex items-center justify-center flex-col gap-5">
         <h1 className="text-[60px] font-bold text-blue-800">هدیه عزیزی</h1>
         <div className="text-center leading-10 text-xl h-[100px]">
           <TypeEffect />
