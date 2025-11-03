3cf9a210a2c073cfde47995e2c47697714e4c59b
Hedye Azizi <hedyeazizi0752@gmail.com>
2025-11-03 23:47:48 +0330

    feat: add Skills route to App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
+import Skills from "./pages/Skills";
 
 function App() {
   return (
@@ -9,6 +10,7 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
             <Route index element={<Home />} />
+            <Route path="/skills" element={<Skills />} />
           </Route>
         </Routes>
       </BrowserRouter>
